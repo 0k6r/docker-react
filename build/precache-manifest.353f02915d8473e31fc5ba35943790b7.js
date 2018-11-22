eb013a7fcdbff6ebab0e989004a507b97b4ae846
Konsatntin Petrov <oku6er@gmail.com>
2018-11-22 10:32:17 +0600

    Remove cached files

---
+++ build/precache-manifest.353f02915d8473e31fc5ba35943790b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c452c4fc5fac65c410f7",
+    "url": "/static/js/main.c452c4fc.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "c452c4fc5fac65c410f7",
+    "url": "/static/css/main.637f054a.chunk.css"
+  },
+  {
+    "revision": "0987c8ba5fd9ab52baf572617eba35ec",
+    "url": "/index.html"
+  }
+];
